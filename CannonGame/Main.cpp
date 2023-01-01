67127f2c29c5ce2d2f65319d5a0de98d35535ecf
Aziz Sufyanov <aziz-sufyanov@mail.ru>
2023-01-01 17:42:33 +0300

    Initial

---
+++ CannonGame/Main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Created: 01.01.23
+ * Author : Aziz Sufyanov
+ * e-mail : aziz-sufyanov@mail.ru
+ *
+ */
+
+
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+	return 0;
+}
